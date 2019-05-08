2162ee79cc37102e4a7c796c02be37a66763822d
mwenge <robhogan@gmail.com>
2019-05-08 11:01:55 +0100

    Update LinearAExplorer.js

--- LinearAExplorer.js
+++ LinearAExplorer.js
@@ -140,7 +140,7 @@ function clearHighlight(evt, name, index) {
   }
 }
 function loadExplorer() {
-  for (var i = 0; i < inscriptions.length; i++) {
+  for (var i = 0; i < 10; i++) {
     loadInscription(inscriptions[i]);
   }
 }
