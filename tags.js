53749cbac69e0cc8aec902fcd098cc309ed039a4
Robert Hogan <robhogan@gmail.com>
2020-01-04 20:09:34 +0000

    Fix 2 images add tags

--- tags.js
+++ tags.js
@@ -1,4 +1,9 @@
 var tags = new Map([
+  ["HT102", ['wrong total']],
   ["HT118", ['wrong total']],
+  ["HT11a", ['wrong total']],
+  ["HT13", ['wrong total']],
+  ["HT9a", ['wrong total']],
+  ["ZA15b", ['wrong total']],
   ["HT119", ['wrong total']],
 ]);
