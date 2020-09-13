8e48e6f4c1a510975ca0cdd0799fb95027685148
Robert Hogan <robhogan@gmail.com>
2020-09-13 14:33:09 +0100

    Add PWA

---
+++ serviceworker.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var CACHE_NAME = 'LinearB';
+var urlsToCache = [
+  'LinearBInscriptions.js',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
