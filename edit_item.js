675e767432a68dbf5611d18729ecf9753ae532d4
Robert Hogan <robhogan@gmail.com>
2025-08-06 20:30:42 +0100

    Make some item properties editable

---
+++ edit_item.js
@@ -0,0 +1,167 @@
+document.querySelectorAll('commentaries > .edit').forEach( x =>  {
+  x.addEventListener("click", event => { 
+    var c = document.querySelector('commentaries');
+    var commentary = document.createElement("commentary");
+    commentary.setAttribute("author", new Date().toLocaleDateString());
+    var entry = document.createElement("commentary-entry");
+    commentary.appendChild(entry);
+    entry.setAttribute("contenteditable", "true");
+    x.after(commentary);
+    entry.focus();
+  });
+});
+
+/*
+ * Enable showing the reading editor.
+ */
+
+document.querySelectorAll('readings > .edit').forEach( x =>  {
+  x.addEventListener("click", event => { 
+    var r = document.querySelector('reading-spec');
+    r.style.display = (r.style.display == "none") ? "block" : "none";
+    var r = document.querySelector('.save');
+    r.style.display = (r.style.display == "none") ? "block" : "none";
+  });
+});
+
+const readingSpecBox = document.querySelector("reading-spec");
+const transcribedReadingAsciiElement = document.querySelector("transcribed-reading-ascii > reading-text");
+const transcribedReadingUnicodeElement = document.querySelector("transcribed-reading-unicode > reading-text");
+const parsedReadingAsciiElement = document.querySelector("parsed-reading-ascii > reading-text");
+const parsedReadingUnicodeElement = document.querySelector("parsed-reading-unicode > reading-text");
+
+/*
+ Update the readings with the current state of the reading editor.
+ */
+readingSpecBox.oninput = (event) => { 
+  var spec = readingSpecBox.innerText.split('\n').filter(x => x.trim() != "");
+
+  var text = createTranscribedAsciiFromSpec(spec);
+  transcribedReadingAsciiElement.innerHTML = text;
+  var text = createTranscribedAsciiFromSpec(spec_to_unicode(spec));
+  transcribedReadingUnicodeElement.innerHTML = text;
+
+  var text = createParsedAsciiFromSpec(spec);
+  parsedReadingAsciiElement.innerHTML = text;
+  var text = createParsedAsciiFromSpec(spec_to_unicode(spec));
+  parsedReadingUnicodeElement.innerHTML = text;
+
+  enableHighlighting();
+}
+
+/*
+ * Downloading the updated page.
+ */
+addDownloadElement();
+
+function removeDownloadElement() {
+  document.querySelectorAll('.save').forEach( x =>  {
+    x.remove();
+  });
+}
+
+function addDownloadElement() {
+  const wrapper = document.querySelector("itemwrapper");
+  var saveButton = document.createElement("div");
+  saveButton.className = "save";
+  saveButton.innerText = "Download"
+  saveButton.style.display = "none"
+  saveButton.addEventListener("click", download);
+  wrapper.appendChild(saveButton);
+}
+
+function getFileName() {
+  let path = window.location.pathname;
+  let fileName = path.substring(path.lastIndexOf('/') + 1);
+  return fileName
+}
+
+function download() {
+  readingSpecBox.style.display = "none";
+  removeDownloadElement();
+  removeHighlightElements(); // So that they don't clutter up the saved doc.
+  let htmlText = "<!DOCTYPE html>\n" +  document.documentElement.outerHTML;
+  let blob = new Blob([htmlText], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, getFileName());
+  addHighlightElements(); // Add the highlights back again.
+  addDownloadElement();
+}
+
+/*
+ * Convert ascii rendering to unicode.
+ */
+function spec_to_unicode(spec) {
+  return spec.map((s) => {
+    if (s[0] == "#") return s;
+    [r,l,w,syl,status] = s.split(' ');
+    return [r,l,w,ascii_to_ideogram.get(syl),status].join(' ');
+  });
+}
+
+function isNumeric(num){
+  return !isNaN(num)
+}
+
+function isASCII(str) {
+  return /^[\x00-\x7F]*$/.test(str);
+}
+
+function createTranscribedAsciiFromSpec(spec) {
+  output = "\n       <line><word number=\"0\">";
+  pr = null;
+  pw = null;
+  spec.filter((x) => x[0] != '#').forEach( s => {
+    [r,l,w,syl,status] = s.split(' ');
+    if (!pr) {
+        pr = r;
+        pw = w;
+    }
+    if (w != pw) output += "</word>";
+    if (r != pr) {
+        pr = r;
+        output += "</line>\n       <line>";
+        if (w == pw) output += `<word number="${w}">`;
+    }
+    if (w != pw) {
+        pw = w;
+        output += `<word number="${w}">`;
+    }
+    if (isNumeric(syl) && isASCII(syl)) {
+        output += `<number>${syl}</number>`;
+    } else {
+        output += `<ideogram>${syl}</ideogram>`;
+    }
+  });
+  output += "</word></line>";
+  return output;
+}
+
+function createParsedAsciiFromSpec(spec) {
+  output = "\n       <line><word number=\"0\">";
+  pl = null;
+  pw = null;
+  spec.filter((x) => x[0] != '#').forEach( s => {
+    [r,l,w,syl,status] = s.split(' ');
+    if (!pr) {
+        pl = l;
+        pw = w;
+    }
+    if (w != pw) output += "</word>";
+    if (l != pl) {
+        pl = l;
+        output += "</line>\n       <line>";
+    }
+    if (w != pw) {
+        pw = w;
+        output += `<word number="${w}">`;
+    }
+    if (isNumeric(syl) && isASCII(syl)) {
+        output += `<number>${syl}</number>`;
+    } else {
+        output += `<ideogram>${syl}</ideogram>`;
+    }
+  });
+  output += "</word></line>";
+  return output;
+}
+
--- item.js
+++ item.js
@@ -1,19 +1,24 @@
 /*
  * Enable highlighting of words when hovered.
  */
+const unicodeContainer = document.querySelector('parsed-reading-unicode');
 document.querySelectorAll('support,site,findspot,scribe,context').forEach( x =>  {
   const searchURL = `/?search=["${x.innerText.replace('\n', ' ')}"]`
   x.addEventListener("click", event => { window.open(searchURL); event.stopPropagation(); });
 });
 
-const unicodeContainer = document.querySelector('transcribed-reading-unicode');
-document.querySelectorAll('word').forEach( x =>  {
-  x.addEventListener("mouseenter", highlightWords());
-  x.addEventListener("mouseleave", clearHighlight());
-  const word = unicodeContainer.querySelector(`word[number='${x.getAttribute("number")}']`);
-  const searchURL = `/?search=["\\"${word.innerText}\\""]`
-  x.addEventListener("click", event => { window.open(searchURL); event.stopPropagation(); });
-});
+enableHighlighting();
+addHighlightElements();
+
+function enableHighlighting() {
+  document.querySelectorAll('word').forEach( x =>  {
+    x.addEventListener("mouseenter", highlightWords());
+    x.addEventListener("mouseleave", clearHighlight());
+    const word = unicodeContainer.querySelector(`word[number='${x.getAttribute("number")}']`);
+    const searchURL = `/?search=["\\"${word.innerText.replace('\n','')}\\""]`
+    x.addEventListener("click", event => { window.open(searchURL); event.stopPropagation(); });
+  });
+}
 
 function highlightWords(e) {
   const color = "rgba(255, 255, 0, 0.5)";
@@ -37,18 +42,27 @@ function clearHighlight(e) {
   }
 }
 
-document.querySelectorAll('img').forEach( img =>  {
-  const imagePath = new URL(img.src).pathname;
-  const imageLookup = imagePath.substring(imagePath.lastIndexOf('/')+1).split('.')[0];
-  const container = img.parentElement.parentElement;
-  const test_img = new Image();
-  test_img.src = imagePath;
-  test_img.onload = () => {
-    addWordsToImage(imageLookup, img, container, test_img.naturalWidth, test_img.naturalHeight);
-  };
-});
+function removeHighlightElements() {
+  document.querySelectorAll('.letter-highlight').forEach( x =>  {
+    x.remove();
+  });
+}
+
+function addHighlightElements() {
+  document.querySelectorAll('img').forEach( img =>  {
+    const imagePath = new URL(img.src).pathname;
+    const imageLookup = imagePath.substring(imagePath.lastIndexOf('/')+1).split('.')[0];
+    const container = img.parentElement.parentElement;
+    const test_img = new Image();
+    test_img.src = imagePath;
+    test_img.onload = () => {
+      addWordsToImage(imageLookup, img, container, test_img.naturalWidth, test_img.naturalHeight);
+    };
+  });
+}
 
 function addWordsToImage(imageLookup, img, container, naturalWidth, naturalHeight) {
+    // coordinates in image_map.js
     var imageCoords = coordinates[imageLookup];
     if (!imageCoords) {
       return;
